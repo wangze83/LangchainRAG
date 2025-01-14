f093b011aa2067526bdc00a98f21cfc122521b78
wangze1 <wangze1@360.cn>
2025-01-14 23:25:58 +0800

    feat: all features

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from components.log import init_log, log
+from components.env import init_env
+from handlers.rag_search import rag_router
+from handlers.reranking import reranking_router
+
+
+def startup():
+    print("init log")
+    init_log()
+    print("init env")
+    init_env()
+
+    log.info("app start")
+
+
+def shutdown():
+    log.info("app shutdown")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    startup()
+    yield
+    shutdown()
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(rag_router)
+app.include_router(reranking_router)
+
+
+@app.get("/")
+async def root():
+    return {"ping": "pong"}
